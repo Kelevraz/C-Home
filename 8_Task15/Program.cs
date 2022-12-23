82f55a251e6c16a2f1c84ceb7a381b802b67132c
--replace-all <zinovskayak@gmail.com>
2022-12-23 23:34:58 +0100

    решили задачу 8_Task15

--- 8_Task15/Program.cs
+++ 8_Task15/Program.cs
@@ -5,3 +5,19 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите цифру от 1 до 7, где 1 - это понедельник, а 7 - воскресенье");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 1 && number <= 7)
+{
+    Console.WriteLine(Day(number) ? "да" : "нет");
+
+bool Day(int number)
+{
+    return number >= 1 && number <= 5;
+}
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
