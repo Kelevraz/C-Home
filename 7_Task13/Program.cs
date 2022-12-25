68a285d1b3f33c4b766a66df3a73757d47963337
--replace-all <zinovskayak@gmail.com>
2022-12-25 18:52:19 +0100

    попытка решить задачу 7_Task13

--- 7_Task13/Program.cs
+++ 7_Task13/Program.cs
@@ -10,3 +10,23 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
+
+int number = new Random().Next();
+Console.WriteLine($"Число => {number}");
+
+// bool Three(int number)
+// {
+//     return number > 99 && number < 1000;
+// }
+
+if (number > 999)
+{
+    while ({number} / 10);
+}
+else if (number < 100)
+{
+    Console.WriteLine($"В числе {number} нет третьей цифры");
+}
+else{
+    Console.WriteLine($"Третья цифра числа {number} => {digit}");
+}
